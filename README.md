8e84fddeb7ddbf2bae18e73f74f74d68e70859b1
Yufeng <xu.y.63@pg.com>
2025-02-13 14:24:36 +0800

    refactored makefile

---
+++ README.md
@@ -0,0 +1,5 @@
+# MyEmbeddedProject
+
+This project is an embedded C application for the MSPM0L1306 microcontroller, built using the GNU ARM toolchain. The project includes source code, a linker script, and a Makefile to automate the build process.
+
+## Project Structure
--- src/main.c
+++ src/main.c
@@ -2,30 +2,10 @@
 
 #include "ti_msp_dl_config.h"
 
-#include "scheduler.h"
-
 int main(void)
 {
     SYSCFG_DL_init();
 
-    scheduler_init();
-
     while (1) {
-        switch (get_power_mode()) {
-            case HIGH_POWER:
-            scheduler_high_power();
-            break;
-
-            case LOW_POWER:
-            scheduler_low_power();
-            break;
-
-            case POWER_DOWN:
-            scheduler_power_down();
-            break;
-
-            default:
-            break;
-        }
     }
 }
